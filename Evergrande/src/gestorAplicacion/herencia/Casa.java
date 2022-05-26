9a0936ec8845a51c0d9fb080877349211b7fe40b
Julián Orozco V <jorozcova@unal.edu.co>
2022-05-26 18:12:27 -0500

    Casa

---
+++ Evergrande/src/gestorAplicacion/herencia/Casa.java
@@ -0,0 +1,51 @@
+package gestorAplicacion.herencia;
+
+public class Casa extends Inmueble {
+    private int numHabitaciones;
+    private int numBanos;
+    private int patio;
+    private int balcon;
+
+
+    public Casa(double precio, String direccion, double area,int numHabitaciones, int numBanos, int patio, int balcon) {
+        super(precio, direccion, area);
+        this.numHabitaciones = numHabitaciones;
+        this.numBanos = numBanos;
+        this.patio = patio;
+        this.balcon = balcon;
+    }
+    
+    public int getNumHabitaciones() {
+        return this.numHabitaciones;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public int getNumBanos() {
+        return this.numBanos;
+    }
+
+    public void setNumBanos(int numBanos) {
+        this.numBanos = numBanos;
+    }
+
+    public int getPatio() {
+        return this.patio;
+    }
+
+    public void setPatio(int patio) {
+        this.patio = patio;
+    }
+
+    public int getBalcon() {
+        return this.balcon;
+    }
+
+    public void setBalcon(int balcon) {
+        this.balcon = balcon;
+    }
+
+    
+}
